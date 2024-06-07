d1bc8e36784015a4058ad30e10515e93528ee385
Upendra Amgain <upendraamgain@gmail.com>
2024-06-07 14:27:58 +0545

    setting up operators

---
+++ js/interaction/sass.js
@@ -0,0 +1,4 @@
+console.log("hello upendra");
+const year = Number(prompt("What is your birth year?"));
+const age = 2024 - year;
+console.log(age);
--- js/variables/index.js
+++ js/variables/index.js
@@ -1,1 +1,6 @@
 console.log("hello world");
+var person = "upendra";
+let person = "suresh";
+const personName = "jhon";
+console.log("person");
+alert("good morining upendra");
