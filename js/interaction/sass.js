163165c60e7451e4f615cfb8cdc7f2794eaa9207
Upendra Amgain <upendraamgain@gmail.com>
2024-06-07 19:56:30 +0545

    operators

--- js/interaction/sass.js
+++ js/interaction/sass.js
@@ -1,4 +1,23 @@
 console.log("hello upendra");
-const year = Number(prompt("What is your birth year?"));
-const age = 2024 - year;
-console.log(age);
+//const year = Number(prompt("What is your birth year?"));
+//const age = 2024 - year;
+//console.log(age);
+//const name = prompt("What is your name?");
+//name === "upendra" ? alert("Welcome admin") : alert("Welcome guest");
+const day = prompt("What is the day today?").toLowerCase(); // Convert to lowercase for case-insensitive comparison
+
+switch (day) {
+  case "monday":
+  case "tuesday":
+  case "wednesday":
+  case "thursday":
+  case "friday":
+    console.log("Weekday");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("Weekend");
+    break;
+  default:
+    console.log("Invalid day");
+}
