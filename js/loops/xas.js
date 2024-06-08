3a2a39371eb26010bb756b6f5e22e5ee16fef2ec
Upendra Amgain <upendraamgain@gmail.com>
2024-06-08 09:13:51 +0545

    loop closed

--- js/loops/xas.js
+++ js/loops/xas.js
@@ -1,3 +1,50 @@
-for (let i=1;i<=100;i++){
-    console.log(i)
-}
+// for (let i=1;i<=100;i++){
+//     console.log(i)
+// }
+// let i = 1;
+// while (i <= 100) {
+//   console.log(i);
+//   i++;
+// }
+// let i = 1;
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 100);
+// writing program for multiplication table for 3 using diffrents loops
+//for
+// for (let i = 1; i <= 10; i++) {
+//   let product = 3 * i;
+
+//   console.log(`3 x ${i} = ${product}`);
+// }
+//while
+// let i = 1;
+
+// while (i <= 10) {
+//   let product = 3 * i;
+
+//   console.log(`3 x ${i} = ${product}`);
+
+//   i++;
+// }
+// do while
+// let i = 1;
+
+// do {
+//   let product = 3 * i;
+
+//   console.log(`3 x ${i} = ${product}`);
+
+//   i++;
+// } while (i <= 10);
+//write  a program for printinng only even numbers  using do while
+let i = 1;
+
+do {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+
+  i++;
+} while (i <= 10);
