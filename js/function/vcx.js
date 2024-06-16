02b4775bb1a360b70b250e523d833b9f8119cbab
Upendra Amgain <upendraamgain@gmail.com>
2024-06-16 21:31:22 +0545

    Deeply Compare Two Objects

--- js/function/vcx.js
+++ js/function/vcx.js
@@ -319,3 +319,36 @@ const exampleMap = new Map([
   ["c", 3],
 ]);
 console.log(mapToObject(exampleMap));
+//Function to Deeply Compare Two Objects
+const deepCompare = (obj1, obj2) => {
+  if (obj1 === obj2) return true;
+
+  if (
+    typeof obj1 !== "object" ||
+    typeof obj2 !== "object" ||
+    obj1 == null ||
+    obj2 == null
+  ) {
+    return false;
+  }
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false;
+
+  for (const key of keys1) {
+    if (!keys2.includes(key) || !deepCompare(obj1[key], obj2[key])) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// Example usage:
+const deepObj1 = { a: 1, b: { c: 3, d: 4 } };
+const deepObj2 = { a: 1, b: { c: 3, d: 4 } };
+const deepObj3 = { a: 1, b: { c: 3, d: 5 } };
+console.log(deepCompare(deepObj1, deepObj2));
+console.log(deepCompare(deepObj1, deepObj3));
